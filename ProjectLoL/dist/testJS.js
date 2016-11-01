50dad4bb699cbff8ce61463f111f67927bb763e8
Ketill Guðmundsson <ketillgudmunds+github@gmail.com>
2016-11-01 12:47:17 +0000

    Fixaði parserinn
    kom í ljós að 3.10 === 3.1 ......

--- ProjectLoL/dist/testJS.js
+++ ProjectLoL/dist/testJS.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* eslint linebreak-style: ["error", "windows"]*/
 (function () {
 
   /****
@@ -34,150 +35,163 @@
   var filteredDB = [];
   var testDB = [];
 
-  // Bæta evenlistener á formið
+  //  Bæta evenlistener á formið
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
-
-  // Handler fyrir files
+  console.time('fileScanner');
+  console.log('fileScanner started');
+  //  Handler fyrir files
   function handleFileSelect(evt) {
-    var files = evt.target.files; // FileList object
-
+    var files = evt.target.files; //  FileList object
+    console.timeEnd('fileScanner');
     dataMinify(files);
   }
 
   function dataMinify(files) {
-    // files is a FileList of File objects. List some properties.
+    //  files is a FileList of File objects. List some properties.
     var numOfFiles = 0;
     var counter = 0;
-
-    // loopum i gegnum alla filea sem ad vid fengum
-    console.time('readerinn');
+    console.time('lestur');
+    console.log('lestur hefst');
+    //  loopum i gegnum alla filea sem ad vid fengum
 
     var _loop = function _loop(f) {
       var reader = new FileReader();
 
-      // thegar ad skjal er loadad keyrum adgerd
+      //  thegar ad skjal er loadad keyrum adgerd
       reader.onload = function () {
         var text = reader.result;
-
-        // splittum a new line
+        //  splittum a new line
         var textArray = text.split('\n');
 
-        // Saekjum thau gogn sem vid hofum ahuga a
+        //  Saekjum thau gogn sem vid hofum ahuga a
         var reducedArray = textArray.filter(function (input) {
           return input.includes('GAMESTATE_GAMELOOP Begin') || input.includes('"exit_code":"EXITCODE') || input.includes('Spawning champion') || input.includes('Build Version: Version') || input.includes('The Killer was');
         });
 
-        // sækjum dagsetningu, og þvingum hana fremst í array
-        var dateIndex = textArray[0].indexOf("started at") + 11;
+        //  sækjum dagsetningu, og þvingum hana fremst í array
+        var dateIndex = textArray[0].indexOf('started at') + 11;
         var date = stringToDate(textArray[0].substring(dateIndex, dateIndex + 19));
         reducedArray.unshift(date);
 
         testDB.push(reducedArray);
-        counter++;
-        if (counter % 200 === 0) {
-          console.log(counter);
-        }
+        counter += 1;
+        /*if (counter % 20 === 0) {
+            console.log('yo, we at file nr: ' + counter);
+        }*/
         if (counter === numOfFiles) {
-          console.timeEnd('readerinn');
+          console.timeEnd('lestur');
           procTest();
         }
       };
       reader.readAsText(f);
     };
 
-    for (var f; f = files[numOfFiles]; numOfFiles++) {
+    for (var f; f = files[numOfFiles]; numOfFiles += 1) {
       _loop(f);
     }
   }
 
-  // Formatta streng og typecast yfir a date format
+  //  Formatta streng og typecast yfir a date format
   function stringToDate(stringDate) {
     stringDate = stringDate.replace('T', ' ').replace(/-/g, '/');
     return new Date(stringDate);
   }
 
-  /****
+  /**
   Hérna hefst keyrsla á úrvinnslufallinu
-  ****/
+  **/
 
   function procTest() {
-    console.time('timer');
-    console.log('timer started');
+    console.time('vinnslutimer');
+    console.log('vinnsla hefst');
     testDB.forEach(function (textFile) {
-
-      //býr til infofylkið
-      var infoArray = {
-        date: textFile.shift(),
-        patch: null,
-        players: [],
-        bot_count: 0,
-        loading_time: null,
-        game_time: null,
-        deaths: [],
-        game_result: null
-      };
-      //infoArray.patch
-      var patchLine = textFile.shift();
-      var patchStart = patchLine.substring(patchLine.indexOf('Build Version: Version') + 23);
-      infoArray.patch = patchStart.substring(0, patchStart.indexOf('.', 3));
-
-      //infoArray.players
-      var summonerArray = textFile.filter(function (input) {
-        return input.includes('Spawning champion');
-      });
-      summonerArray.forEach(function (item) {
-        var tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-        item = item.substring(item.indexOf(')') + 1);
-        var tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
-        var tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
-        var tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-        item = item.substring(item.indexOf(')') + 1);
-        var tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-        if (tempType == 'is BOT AI') {
-          infoArray.bot_count++;
-        } else {
-          var playerArray = {
-            champion: null,
-            skin: null,
-            team: null,
-            summonername: null,
-            playertype: null
+      // býr til infofylkið
+      try {
+        (function () {
+          var infoArray = {
+            date: textFile.shift(),
+            patch: null,
+            players: [],
+            bot_count: 0,
+            loading_time: null,
+            game_time: null,
+            deaths: [],
+            game_result: null
           };
-          playerArray.champion = tempChamp;
-          playerArray.skin = tempSkin;
-          playerArray.team = tempTeam;
-          playerArray.summonername = tempName;
-          playerArray.playertype = tempType;
-          infoArray.players.push(playerArray);
-        }
-      });
+          // infoArray.patch
+          var patchLine = textFile.shift();
+          var patchStart = patchLine.substring(patchLine.indexOf('Build Version: Version') + 23);
+          var patchString = patchStart.substring(0, patchStart.indexOf('.', 3));
+          var patchSplit = patchString.split('.');
+          infoArray.patch = patchString - 0;
+          // console.log(patchSplit);
+          if (patchSplit[0] > 3 || patchSplit[0] === 3 || patchSplit[1] > 9) {
+            // infoArray.players
+            var summonerArray = textFile.filter(function (input) {
+              return input.includes('Spawning champion');
+            });
+            summonerArray.forEach(function (item) {
+              var tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+              item = item.substring(item.indexOf(')') + 1);
+              var tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
+              var tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
+              var tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+              item = item.substring(item.indexOf(')') + 1);
+              var tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+              if (tempType === 'is BOT AI') {
+                infoArray.bot_count += 1;
+              } else {
+                var playerArray = {
+                  // k
+                  champion: null,
+                  skin: null,
+                  team: null,
+                  summonername: null,
+                  playertype: null
+                };
+                playerArray.champion = tempChamp;
+                playerArray.skin = tempSkin;
+                playerArray.team = tempTeam;
+                playerArray.summonername = tempName;
+                playerArray.playertype = tempType;
+                infoArray.players.push(playerArray);
+              }
+            });
 
-      //infoArray.loading_time
-      infoArray.loading_time = textFile.filter(function (input) {
-        return input.includes('GAMESTATE_GAMELOOP Begin');
-      })[0].substring(0, 10) - 0;
+            // infoArray.loading_time
+            infoArray.loading_time = textFile.filter(function (input) {
+              return input.includes('GAMESTATE_GAMELOOP Begin');
+            })[0].substring(0, 10) - 0;
 
-      //infoArray.game_time && infoArray.game_result
-      var gameEndLine = textFile.filter(function (input) {
-        return input.includes('"exit_code":"EXITCODE');
-      })[0];
-      //infoArray.game_time
-      infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
-      //infoArray.game_result
-      var gameResultCarNr = gameEndLine.indexOf('"Game exited","exit_code":"');
-      infoArray.game_result = gameEndLine.substring(gameResultCarNr + 36, gameResultCarNr + 37);
+            // infoArray.game_time && infoArray.game_result
+            var gameEndLine = textFile.filter(function (input) {
+              return input.includes('"exit_code":"EXITCODE');
+            })[0];
+            // infoArray.game_time
+            infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
+            // infoArray.game_result
+            var gameResultCarNr = gameEndLine.indexOf('"Game exited","exit_code":"');
 
-      //infoArray.deaths
-      var deathArray = textFile.filter(function (input) {
-        return input.includes('The Killer was');
-      });
-      deathArray.forEach(function (item) {
-        var deathTime = item.substring(0, 10) - infoArray.loading_time;
-        infoArray.deaths.push(deathTime);
-      });
-      filteredDB.push(infoArray);
+            infoArray.game_result = gameEndLine.substring(gameResultCarNr + 36, gameResultCarNr + 37);
+
+            // infoArray.deaths
+            var deathArray = textFile.filter(function (input) {
+              return input.includes('The Killer was');
+            });
+            deathArray.forEach(function (item) {
+              var deathTime = item.substring(0, 10) - infoArray.loading_time;
+              infoArray.deaths.push(deathTime);
+            });
+            filteredDB.push(infoArray);
+          } else {
+            console.log('file to old');
+          }
+        })();
+      } catch (err) {
+        console.log('error');
+      }
     });
     console.log(filteredDB);
-    console.timeEnd('timer');
+    console.timeEnd('vinnslutimer');
   }
 })();
--- ProjectLoL/src/testJS.js
+++ ProjectLoL/src/testJS.js
@@ -35,11 +35,12 @@
 
   //  Bæta evenlistener á formið
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
-
+  console.time('fileScanner');
+  console.log('fileScanner started');
   //  Handler fyrir files
   function handleFileSelect(evt) {
     let files = evt.target.files; //  FileList object
-
+    console.timeEnd('fileScanner');
     dataMinify(files);
   }
 
@@ -47,7 +48,8 @@
     //  files is a FileList of File objects. List some properties.
     let numOfFiles = 0;
     let counter = 0;
-
+    console.time('lestur');
+    console.log('lestur hefst');
     //  loopum i gegnum alla filea sem ad vid fengum
     for (let f; f = files[numOfFiles]; numOfFiles += 1) {
       const reader = new FileReader();
@@ -55,7 +57,6 @@
       //  thegar ad skjal er loadad keyrum adgerd
       reader.onload = function () {
         const text = reader.result;
-
         //  splittum a new line
         const textArray = text.split('\n');
 
@@ -75,7 +76,12 @@
 
         testDB.push(reducedArray);
         counter += 1;
+        /*if (counter % 20 === 0) {
+
+          console.log('yo, we at file nr: ' + counter);
+        }*/
         if (counter === numOfFiles) {
+          console.timeEnd('lestur');
           procTest();
         }
       };
@@ -94,85 +100,95 @@
   **/
 
   function procTest() {
+    console.time('vinnslutimer');
+    console.log('vinnsla hefst');
     testDB.forEach(function (textFile) {
       // býr til infofylkið
-      const infoArray = {
-        date: textFile.shift(),
-        patch: null,
-        players: [],
-        bot_count: 0,
-        loading_time: null,
-        game_time: null,
-        deaths: [],
-        game_result: null,
-      };
-      // infoArray.patch
-      const patchLine = textFile.shift();
-      const patchStart = patchLine.substring(patchLine.indexOf('Build Version: Version') + 23);
-      const patchString = patchStart.substring(0, patchStart.indexOf('.', 3));
-      const patchSplit = patchString.split('.');
-      infoArray.patch = patchString - 0;
-      if (patchSplit[0] > 3 || (patchSplit[0] === 3 || patchSplit[1] > 9)) {
-        // infoArray.players
-        const summonerArray = textFile.filter(function (input) {
-          return (input.includes('Spawning champion'));
-        });
-        summonerArray.forEach(function (item) {
-          const tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-          item = item.substring(item.indexOf(')') + 1);
-          const tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
-          const tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
-          const tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-          item = item.substring(item.indexOf(')') + 1);
-          const tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-          if (tempType === 'is BOT AI') {
-            infoArray.bot_count += 1;
-          } else {
-            const playerArray = {
-              // k
-              champion: null,
-              skin: null,
-              team: null,
-              summonername: null,
-              playertype: null,
-            };
-            playerArray.champion = tempChamp;
-            playerArray.skin = tempSkin;
-            playerArray.team = tempTeam;
-            playerArray.summonername = tempName;
-            playerArray.playertype = tempType;
-            infoArray.players.push(playerArray);
-          }
-        });
+      try {
+        const infoArray = {
+          date: textFile.shift(),
+          patch: null,
+          players: [],
+          bot_count: 0,
+          loading_time: null,
+          game_time: null,
+          deaths: [],
+          game_result: null,
+        };
+        // infoArray.patch
+        const patchLine = textFile.shift();
+        const patchStart = patchLine.substring(patchLine.indexOf('Build Version: Version') + 23);
+        const patchString = patchStart.substring(0, patchStart.indexOf('.', 3));
+        const patchSplit = patchString.split('.');
+        infoArray.patch = patchString - 0;
+        // console.log(patchSplit);
+        if (patchSplit[0] > 3 || (patchSplit[0] === 3 || patchSplit[1] > 9)) {
+          // infoArray.players
+          const summonerArray = textFile.filter(function (input) {
+            return (input.includes('Spawning champion'));
+          });
+          summonerArray.forEach(function (item) {
+            const tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+            item = item.substring(item.indexOf(')') + 1);
+            const tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
+            const tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
+            const tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+            item = item.substring(item.indexOf(')') + 1);
+            const tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+            if (tempType === 'is BOT AI') {
+              infoArray.bot_count += 1;
+            } else {
+              const playerArray = {
+                // k
+                champion: null,
+                skin: null,
+                team: null,
+                summonername: null,
+                playertype: null,
+              };
+              playerArray.champion = tempChamp;
+              playerArray.skin = tempSkin;
+              playerArray.team = tempTeam;
+              playerArray.summonername = tempName;
+              playerArray.playertype = tempType;
+              infoArray.players.push(playerArray);
+            }
+          });
 
-        // infoArray.loading_time
-        infoArray.loading_time = textFile.filter(function (input) {
-          return (input.includes('GAMESTATE_GAMELOOP Begin'));
-        })[0].substring(0, 10) - 0;
+          // infoArray.loading_time
+          infoArray.loading_time = textFile.filter(function (input) {
+            return (input.includes('GAMESTATE_GAMELOOP Begin'));
+          })[0].substring(0, 10) - 0;
 
-        // infoArray.game_time && infoArray.game_result
-        const gameEndLine = textFile.filter(function (input) {
-          return (input.includes('"exit_code":"EXITCODE'));
-        })[0];
-        // infoArray.game_time
-        infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
-        // infoArray.game_result
-        const gameResultCarNr = gameEndLine.indexOf('"Game exited","exit_code":"');
-        infoArray.game_result = gameEndLine.substring(gameResultCarNr + 36, gameResultCarNr + 37);
+          // infoArray.game_time && infoArray.game_result
+          const gameEndLine = textFile.filter(function (input) {
+            return (input.includes('"exit_code":"EXITCODE'));
+          })[0];
+          // infoArray.game_time
+          infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
+          // infoArray.game_result
+          const gameResultCarNr = gameEndLine.indexOf('"Game exited","exit_code":"');
 
-        // infoArray.deaths
-        const deathArray = textFile.filter(function (input) {
-          return (input.includes('The Killer was'));
-        });
-        deathArray.forEach(function (item) {
-          const deathTime = item.substring(0, 10) - infoArray.loading_time;
-          infoArray.deaths.push(deathTime);
-        });
-        filteredDB.push(infoArray);
-      } else {
-        console.log('homo');
+          infoArray.game_result = gameEndLine.substring(gameResultCarNr + 36, gameResultCarNr + 37);
+
+          // infoArray.deaths
+          const deathArray = textFile.filter(function (input) {
+            return (input.includes('The Killer was'));
+          });
+          deathArray.forEach(function (item) {
+            const deathTime = item.substring(0, 10) - infoArray.loading_time;
+            infoArray.deaths.push(deathTime);
+          });
+          filteredDB.push(infoArray);
+        } else {
+          console.log('file to old');
+        }
+      }
+      catch(err) {
+        console.log('error');
       }
     });
     console.log(filteredDB);
+    console.timeEnd('vinnslutimer');
   }
 }());
