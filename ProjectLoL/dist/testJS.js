a19d6751242d517385c9edb3621d3aac279b06c5
Ketill Guðmundsson <ketillgudmunds+github@gmail.com>
2016-11-01 20:43:04 +0000

    Bætti við helling af vinnslu

--- ProjectLoL/dist/testJS.js
+++ ProjectLoL/dist/testJS.js
@@ -124,65 +124,66 @@
           var patchString = patchStart.substring(0, patchStart.indexOf('.', 3));
           var patchSplit = patchString.split('.');
           infoArray.patch = patchString - 0;
-          // console.log(patchSplit);
+          // if patch less then 3.10 throw away
           if (patchSplit[0] > 3 || patchSplit[0] === 3 || patchSplit[1] > 9) {
-            // infoArray.players
-            var summonerArray = textFile.filter(function (input) {
-              return input.includes('Spawning champion');
-            });
-            summonerArray.forEach(function (item) {
-              var tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-              item = item.substring(item.indexOf(')') + 1);
-              var tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
-              var tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
-              var tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-              item = item.substring(item.indexOf(')') + 1);
-              var tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-              if (tempType === 'is BOT AI') {
-                infoArray.bot_count += 1;
-              } else {
-                var playerArray = {
-                  // k
-                  champion: null,
-                  skin: null,
-                  team: null,
-                  summonername: null,
-                  playertype: null
-                };
-                playerArray.champion = tempChamp;
-                playerArray.skin = tempSkin;
-                playerArray.team = tempTeam;
-                playerArray.summonername = tempName;
-                playerArray.playertype = tempType;
-                infoArray.players.push(playerArray);
-              }
-            });
-
-            // infoArray.loading_time
-            infoArray.loading_time = textFile.filter(function (input) {
-              return input.includes('GAMESTATE_GAMELOOP Begin');
-            })[0].substring(0, 10) - 0;
-
             // infoArray.game_time && infoArray.game_result
             var gameEndLine = textFile.filter(function (input) {
               return input.includes('"exit_code":"EXITCODE');
             })[0];
-            // infoArray.game_time
-            infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
             // infoArray.game_result
             var gameResultCarNr = gameEndLine.indexOf('"Game exited","exit_code":"');
-
             infoArray.game_result = gameEndLine.substring(gameResultCarNr + 36, gameResultCarNr + 37);
-
-            // infoArray.deaths
-            var deathArray = textFile.filter(function (input) {
-              return input.includes('The Killer was');
-            });
-            deathArray.forEach(function (item) {
-              var deathTime = item.substring(0, 10) - infoArray.loading_time;
-              infoArray.deaths.push(deathTime);
-            });
-            filteredDB.push(infoArray);
+            // if abandoned game, throw away
+            if (infoArray.game_result !== 'A') {
+              // infoArray.players
+              var summonerArray = textFile.filter(function (input) {
+                return input.includes('Spawning champion');
+              });
+              summonerArray.forEach(function (item) {
+                var tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+                item = item.substring(item.indexOf(')') + 1);
+                var tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
+                var tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
+                var tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+                item = item.substring(item.indexOf(')') + 1);
+                var tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+                if (tempType === 'is BOT AI') {
+                  infoArray.bot_count += 1;
+                } else {
+                  var playerArray = {
+                    // k
+                    champion: null,
+                    skin: null,
+                    team: null,
+                    summonername: null,
+                    playertype: null
+                  };
+                  playerArray.champion = tempChamp;
+                  playerArray.skin = tempSkin;
+                  playerArray.team = tempTeam;
+                  playerArray.summonername = tempName;
+                  playerArray.playertype = tempType;
+                  infoArray.players.push(playerArray);
+                }
+              });
+              // infoArray.loading_time
+              infoArray.loading_time = textFile.filter(function (input) {
+                return input.includes('GAMESTATE_GAMELOOP Begin');
+              })[0].substring(0, 10) - 0;
+              // infoArray.game_time
+              infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
+              // infoArray.deaths
+              var deathArray = textFile.filter(function (input) {
+                return input.includes('The Killer was');
+              });
+              deathArray.forEach(function (item) {
+                var deathTime = item.substring(0, 10) - infoArray.loading_time;
+                infoArray.deaths.push(deathTime);
+              });
+              filteredDB.push(infoArray);
+            } else {
+              console.log('abandoned game, not used');
+            }
           } else {
             console.log('file to old');
           }
@@ -193,5 +194,94 @@
     });
     console.log(filteredDB);
     console.timeEnd('vinnslutimer');
+    urvinnsla();
+  }
+
+  // öll föll sem sjá um vinnslu verður kallað úr hér
+  function urvinnsla() {
+    vinnaFylki();
+    console.log(deathAtMinute);
+    console.log(death_and_gameLength_and_result);
+    console.log(loadingTime);
+    console.log(playTime);
+    console.log(winsLosses);
+    console.log(gameByHour);
+    console.log(gameByDay);
+    console.log(gameByMonthDay);
+    console.log(gameByMonth);
+    console.log(gameByYear);
+    console.log(gameByAll);
+  }
+  // array með deaths flokkað eftir sekúndu
+  var deathAtMinute = {};
+  // array sem geymir death og gamelength saman
+  var death_and_gameLength_and_result = [];
+  // geymir total load time
+  var loadingTime = 0;
+  // geymir total play time
+  var playTime = 0;
+  // geymir wins og losses
+  var winsLosses = {
+    W: 0,
+    L: 0
+  };
+  //hvaða dag, viku ár etc... spilaru á
+  var gameByHour = {};
+  var gameByDay = {};
+  var gameByMonthDay = {};
+  var gameByMonth = {};
+  var gameByYear = {};
+  var gameByAll = {};
+
+  // fall sem keyrir í gegnum filteredDB fylkið
+  function vinnaFylki() {
+    // fyrir hvert stak, arrayStak, í fylkinu filteredDB
+    filteredDB.forEach(function (arrayStak) {
+      //fyrir hvern death hvern death
+      arrayStak.deaths.forEach(function (deathStak) {
+        var deathToMinute = formatTime(deathStak)[1];
+        if (!deathAtMinute[deathToMinute]) deathAtMinute[deathToMinute] = 0;
+        ++deathAtMinute[deathToMinute];
+      });
+      //finnur gildi fyrir death_and_gameLength_and_result
+      var deathToLength = [arrayStak.deaths.length, arrayStak.game_time, arrayStak.game_result];
+      death_and_gameLength_and_result.push(deathToLength);
+      // finnur gildi í loadingTime og playTime
+      loadingTime += arrayStak.loading_time;
+      playTime += arrayStak.game_time;
+      winsLosses[arrayStak.game_result] += 1;
+      //
+      gameBy(arrayStak.date);
+    });
+  }
+
+  // hleður inn í gameByX
+  function gameBy(date) {
+    if (!gameByHour[date.getHours()]) gameByHour[date.getHours()] = 0;
+    ++gameByHour[date.getHours()];
+
+    if (!gameByDay[date.getDay()]) gameByDay[date.getDay()] = 0;
+    ++gameByDay[date.getDay()];
+
+    if (!gameByMonthDay[date.getDate()]) gameByMonthDay[date.getDate()] = 0;
+    ++gameByMonthDay[date.getDate()];
+
+    if (!gameByMonth[date.getMonth()]) gameByMonth[date.getMonth()] = 0;
+    ++gameByMonth[date.getMonth()];
+
+    if (!gameByYear[date.getFullYear()]) gameByYear[date.getFullYear()] = 0;
+    ++gameByYear[date.getFullYear()];
+
+    var tempFullDate = [date.getDate(), date.getMonth(), date.getFullYear()];
+    if (!gameByAll[tempFullDate]) gameByAll[tempFullDate] = 0;
+    ++gameByAll[tempFullDate];
+  }
+
+  // breytir sekúndum í formatið [hh, mm, ss]
+  function formatTime(seconds) {
+    var hour = Math.floor(seconds / 3600);
+    var minute = Math.floor((seconds - hour * 3600) / 60);
+    var second = seconds % 60;
+    return [hour, minute, second];
   }
 })();
--- ProjectLoL/src/testJS.js
+++ ProjectLoL/src/testJS.js
@@ -121,65 +121,66 @@
         const patchString = patchStart.substring(0, patchStart.indexOf('.', 3));
         const patchSplit = patchString.split('.');
         infoArray.patch = patchString - 0;
-        // console.log(patchSplit);
+        // if patch less then 3.10 throw away
         if (patchSplit[0] > 3 || (patchSplit[0] === 3 || patchSplit[1] > 9)) {
-          // infoArray.players
-          const summonerArray = textFile.filter(function (input) {
-            return (input.includes('Spawning champion'));
-          });
-          summonerArray.forEach(function (item) {
-            const tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-            item = item.substring(item.indexOf(')') + 1);
-            const tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
-            const tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
-            const tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-            item = item.substring(item.indexOf(')') + 1);
-            const tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
-            if (tempType === 'is BOT AI') {
-              infoArray.bot_count += 1;
-            } else {
-              const playerArray = {
-                // k
-                champion: null,
-                skin: null,
-                team: null,
-                summonername: null,
-                playertype: null,
-              };
-              playerArray.champion = tempChamp;
-              playerArray.skin = tempSkin;
-              playerArray.team = tempTeam;
-              playerArray.summonername = tempName;
-              playerArray.playertype = tempType;
-              infoArray.players.push(playerArray);
-            }
-          });
-
-          // infoArray.loading_time
-          infoArray.loading_time = textFile.filter(function (input) {
-            return (input.includes('GAMESTATE_GAMELOOP Begin'));
-          })[0].substring(0, 10) - 0;
-
           // infoArray.game_time && infoArray.game_result
           const gameEndLine = textFile.filter(function (input) {
             return (input.includes('"exit_code":"EXITCODE'));
           })[0];
-          // infoArray.game_time
-          infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
           // infoArray.game_result
           const gameResultCarNr = gameEndLine.indexOf('"Game exited","exit_code":"');
-
           infoArray.game_result = gameEndLine.substring(gameResultCarNr + 36, gameResultCarNr + 37);
-
-          // infoArray.deaths
-          const deathArray = textFile.filter(function (input) {
-            return (input.includes('The Killer was'));
-          });
-          deathArray.forEach(function (item) {
-            const deathTime = item.substring(0, 10) - infoArray.loading_time;
-            infoArray.deaths.push(deathTime);
-          });
-          filteredDB.push(infoArray);
+          // if abandoned game, throw away
+          if(infoArray.game_result !== 'A') {
+            // infoArray.players
+            const summonerArray = textFile.filter(function (input) {
+              return (input.includes('Spawning champion'));
+            });
+            summonerArray.forEach(function (item) {
+              const tempChamp = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+              item = item.substring(item.indexOf(')') + 1);
+              const tempSkin = item.substring(item.indexOf('skinID') + 7, item.indexOf('skinID') + 8);
+              const tempTeam = item.substring(item.indexOf('team') + 5, item.indexOf('team') + 6);
+              const tempName = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+              item = item.substring(item.indexOf(')') + 1);
+              const tempType = item.substring(item.indexOf('(') + 1, item.indexOf(')'));
+              if (tempType === 'is BOT AI') {
+                infoArray.bot_count += 1;
+              } else {
+                const playerArray = {
+                  // k
+                  champion: null,
+                  skin: null,
+                  team: null,
+                  summonername: null,
+                  playertype: null,
+                };
+                playerArray.champion = tempChamp;
+                playerArray.skin = tempSkin;
+                playerArray.team = tempTeam;
+                playerArray.summonername = tempName;
+                playerArray.playertype = tempType;
+                infoArray.players.push(playerArray);
+              }
+            });
+            // infoArray.loading_time
+            infoArray.loading_time = textFile.filter(function (input) {
+              return (input.includes('GAMESTATE_GAMELOOP Begin'));
+            })[0].substring(0, 10) - 0;
+            // infoArray.game_time
+            infoArray.game_time = gameEndLine.substring(0, 10) - infoArray.loading_time;
+            // infoArray.deaths
+            const deathArray = textFile.filter(function (input) {
+              return (input.includes('The Killer was'));
+            });
+            deathArray.forEach(function (item) {
+              const deathTime = item.substring(0, 10) - infoArray.loading_time;
+              infoArray.deaths.push(deathTime);
+            });
+            filteredDB.push(infoArray);
+          } else {
+            console.log('abandoned game, not used');
+          }
         } else {
           console.log('file to old');
         }
@@ -190,5 +191,101 @@
     });
     console.log(filteredDB);
     console.timeEnd('vinnslutimer');
+    urvinnsla();
+  }
+
+  // öll föll sem sjá um vinnslu verður kallað úr hér
+  function urvinnsla() {
+    vinnaFylki();
+    console.log(deathAtMinute);
+    console.log(death_and_gameLength_and_result);
+    console.log(loadingTime);
+    console.log(playTime);
+    console.log(winsLosses);
+    console.log(gameByHour);
+    console.log(gameByDay);
+    console.log(gameByMonthDay);
+    console.log(gameByMonth);
+    console.log(gameByYear);
+    console.log(gameByAll);
+  }
+  // array með deaths flokkað eftir sekúndu
+  const deathAtMinute = {};
+  // array sem geymir death og gamelength saman
+  const death_and_gameLength_and_result = [];
+  // geymir total load time
+  let loadingTime = 0;
+  // geymir total play time
+  let playTime = 0;
+  // geymir wins og losses
+  const winsLosses = {
+    W: 0,
+    L: 0,
+  };
+  //hvaða dag, viku ár etc... spilaru á
+  const gameByHour = {};
+  const gameByDay = {};
+  const gameByMonthDay = {};
+  const gameByMonth = {};
+  const gameByYear = {};
+  const gameByAll = {};
+
+  // fall sem keyrir í gegnum filteredDB fylkið
+  function vinnaFylki() {
+    // fyrir hvert stak, arrayStak, í fylkinu filteredDB
+    filteredDB.forEach(function (arrayStak) {
+      //fyrir hvern death hvern death
+      arrayStak.deaths.forEach(function (deathStak) {
+        const deathToMinute = formatTime(deathStak)[1];
+        if(!deathAtMinute[deathToMinute])
+          deathAtMinute[deathToMinute] = 0;
+        ++deathAtMinute[deathToMinute];
+      });
+      //finnur gildi fyrir death_and_gameLength_and_result
+      const deathToLength = [arrayStak.deaths.length, arrayStak.game_time, arrayStak.game_result];
+      death_and_gameLength_and_result.push(deathToLength)
+      // finnur gildi í loadingTime og playTime
+      loadingTime += arrayStak.loading_time;
+      playTime += arrayStak.game_time;
+      winsLosses[arrayStak.game_result] += 1;
+      //
+      gameBy(arrayStak.date);
+    });
+  }
+
+  // hleður inn í gameByX
+  function gameBy(date) {
+    if(!gameByHour[date.getHours()])
+      gameByHour[date.getHours()] = 0;
+    ++gameByHour[date.getHours()];
+
+    if(!gameByDay[date.getDay()])
+      gameByDay[date.getDay()] = 0;
+    ++gameByDay[date.getDay()];
+
+    if(!gameByMonthDay[date.getDate()])
+      gameByMonthDay[date.getDate()] = 0;
+    ++gameByMonthDay[date.getDate()];
+
+    if(!gameByMonth[date.getMonth()])
+      gameByMonth[date.getMonth()] = 0;
+    ++gameByMonth[date.getMonth()];
+
+    if(!gameByYear[date.getFullYear()])
+      gameByYear[date.getFullYear()] = 0;
+    ++gameByYear[date.getFullYear()];
+
+    const tempFullDate = [date.getDate(), date.getMonth(), date.getFullYear()];
+    if(!gameByAll[tempFullDate])
+      gameByAll[tempFullDate] = 0;
+    ++gameByAll[tempFullDate];
+  }
+
+  // breytir sekúndum í formatið [hh, mm, ss]
+  function formatTime(seconds) {
+    const hour = Math.floor(seconds / 3600);
+    const minute = Math.floor((seconds - hour * 3600) / 60);
+    const second = seconds % 60;
+    return [hour, minute, second];
   }
 }());
