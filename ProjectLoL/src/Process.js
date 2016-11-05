3b5a148d243262e51faa32270c3ed11e2abcb0c8
Ketill Guðmundsson <ketillgudmunds+github@gmail.com>
2016-11-05 16:49:05 +0000

    Virkandi API call til að fá champ list

--- ProjectLoL/src/Process.js
+++ ProjectLoL/src/Process.js
@@ -1,4 +1,22 @@
+const API_KEY = 'RGAPI-f4d249c4-0cdd-4aa1-8307-3ec5164f0829';
 console.log('ja');
 function urvinnsla() {
-  console.log('ja');
+  Api_Call();
+}
+
+function Api_Call() {
+  $.ajax({
+      url: 'https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=info&api_key=' + API_KEY,
+      //    https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=info&api_key=RGAPI-f4d249c4-0cdd-4aa1-8307-3ec5164f0829
+      type: 'GET',
+      dataType: 'json',
+      data: {
+      },
+      success: function (json) {
+        console.log(json);
+      },
+      error: function (XMLHttpRequest, textStatus, errorThrown) {
+          alert("error getting connecting to API");
+      }
+  });
 }
